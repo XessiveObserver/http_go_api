f83b0a1aabc772ae4d00b3171d73f2e930a2aa24
XessiveObserver <xessiveobserver@gmail.com>
2023-10-02 09:29:12 +0300

    moved routes from main to handlers

---
+++ handlers.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func getUsers(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+
+	rows, err := DB.Query("SELECT * FROM coders")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var users []User
+	for rows.Next() {
+		var user User
+		if err := rows.Scan(&user.ID, &user.Name, &user.Email); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		users = append(users, user)
+	}
+
+	json.NewEncoder(w).Encode(users)
+}
+
+// getUser retrieves a specific user from the database by ID and returns it as JSON
+func getUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+	id, _ := strconv.Atoi(ps.ByName("id"))
+
+	var user User
+	err := DB.QueryRow("SELECT * FROM coders WHERE id = $1", id).Scan(&user.ID, &user.Name, &user.Email)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, "User not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
+
+// createUser inserts a new user into the database and returns the created user as JSON
+func createUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+
+	// Insert the new user into the database
+	err := DB.QueryRow("INSERT INTO coders (name, email) VALUES ($1, $2) RETURNING id", user.Name, user.Email).Scan(&user.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
+
+// updateUser updates a user in the database by ID and returns the updated user as JSON
+func updateUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+	id, _ := strconv.Atoi(ps.ByName("id"))
+
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+
+	// Update the user in the database
+	_, err := DB.Exec("UPDATE coders SET name = $1, email = $2 WHERE id = $3", user.Name, user.Email, id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	user.ID = id
+	json.NewEncoder(w).Encode(user)
+}
+
+// deleteUser deletes a user from the database by ID and returns the deleted user as JSON
+func deleteUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+	id, _ := strconv.Atoi(ps.ByName("id"))
+
+	var user User
+	err := DB.QueryRow("DELETE FROM coders WHERE id = $1 RETURNING id, name, email", id).Scan(&user.ID, &user.Name, &user.Email)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, "User not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
